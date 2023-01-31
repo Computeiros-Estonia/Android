c13fa9a5d434164e9f1cffb6a0210d4c9f3ddb29
gregoSFL <85593794+gregoSFL@users.noreply.github.com>
2023-01-31 00:44:46 -0300

    Add files via upload

---
+++ Library/app/src/main/java/com/estoniaTech/library/MainPage.kt
@@ -0,0 +1,11 @@
+package com.estoniaTech.library
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainPage : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main_page)
+    }
+}
